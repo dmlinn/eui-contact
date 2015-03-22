3bb7f9f0ea45336eadf6718fb18b254cd1c6329d
Daniel Linn <dmlinn@gmail.com>
2015-03-21 20:21:05 -0600

    Troubleshooting deployment issues.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var path = require('path');
+
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
@@ -10,12 +12,8 @@ app.set('view engine', 'ejs');
 
 app.engine('html', require('ejs').renderFile);
 
-// set the home page route
-app.get('/', function(req, res) {
+app.use(express.static(__dirname + '/app'));
 
-    // ejs render automatically looks in the views folder
-    app.use(express.static(__dirname + '/app'));
-});
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
